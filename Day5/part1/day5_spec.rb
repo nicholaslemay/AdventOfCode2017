c8c6a0f64053ea46ffedbe76d3cf1a2e01965c1c
nicholas lemay <lemaynicholas@gmail.com>
2017-12-05 20:31:33 -0500

    Day 5, part 1 in Ruby

---
+++ Day5/part1/day5_spec.rb
@@ -0,0 +1,40 @@
+
+class MazeCpu
+  attr_reader :steps
+
+  def initialize(instructions)
+    @instructions = instructions
+    @current_position = 0  
+    @steps = 0
+  end
+  
+  def execute
+    while(@current_position < @instructions.count)
+       movement = @instructions[@current_position]
+       @instructions[@current_position] +=1
+       @current_position += movement
+       @steps +=1
+    end
+    
+  end 
+
+end
+
+describe 'MazeCpu' do
+
+  it 'can exit the sample maze in 5 steps' do
+    instructions = [0,3,0,1,-3]
+    cpu = MazeCpu.new(instructions)
+    cpu.execute
+    
+    expect(cpu.steps).to eq(5)
+  end
+
+  it 'can exit the sample maze from the input' do
+    instructions = File.new('input.txt').readlines.collect{|r| r.strip.to_i}
+    cpu = MazeCpu.new(instructions)
+    cpu.execute
+    
+    expect(cpu.steps).to eq(373160)
+  end
+end
